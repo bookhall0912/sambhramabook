1415fea29d62affe7baa7c8ff9be905dfd2449d5
hallbook0912 <hallbook0912@gmail.com>
2025-12-23 22:09:43 +0530

    Prod changes

--- apiNew/SambhramaBook.Api/obj/Debug/net10.0/SambhramaBook.Api.AssemblyInfo.cs
+++ apiNew/SambhramaBook.Api/obj/Debug/net10.0/SambhramaBook.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SambhramaBook.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73e36ef2e9b75c0d68103eb2bb7fa55d02cb26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5503b95342a2382398075678a8702b1a829b839")]
 [assembly: System.Reflection.AssemblyProductAttribute("SambhramaBook.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SambhramaBook.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
