8b75e2b7cc2bc24f2a83fcfc31d2b310d769f788
hallbook0912 <hallbook0912@gmail.com>
2025-12-24 11:26:34 +0530

    Cors

--- ui/environments/environment.production.ts
+++ ui/environments/environment.production.ts
@@ -5,8 +5,7 @@ export const environment: Environment = {
   // Set this to your production API URL
   // If using Azure Functions, use: '/api' for relative path
   // If using separate API service, use full URL: 'https://your-api.azurewebsites.net'
-  apiUrl: '/api',
+  apiUrl: 'http://dev-api.sambhramabook.com/api',
   // Add your Geoapify API key for production
-  geoapifyApiKey: undefined
+  geoapifyApiKey: undefined,
 };
-
